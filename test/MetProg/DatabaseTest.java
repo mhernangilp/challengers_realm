810861af5e76984ae651b5585a893efd9ec46289
Marcos Hernangil <mhernangilp@gmail.com>
2023-05-11 20:53:41 +0200

    added test

---
+++ test/MetProg/DatabaseTest.java
@@ -0,0 +1,85 @@
+
+package MetProg;
+
+import java.util.ArrayList;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class DatabaseTest {
+    
+    
+    public DatabaseTest() {
+        
+    }
+
+    @Test
+    public void testPedirPersonaje() throws Exception {
+        
+    }
+
+    @Test
+    public void testGetUsuarios() {
+        
+    }
+
+    @Test
+    public void testGetPersonaje() {
+        
+    }
+
+    @Test
+    public void testGetDesafios() {
+        
+    }
+
+    @Test
+    public void testSaveData() throws Exception {
+        
+    }
+
+    @Test
+    public void testInicializarPersonajes() {
+        
+    }
+
+    @Test
+    public void testEliminarUsuario() {
+        
+    }
+
+    @Test
+    public void testAñadirUsuario() {
+        
+    }
+
+    @Test
+    public void testGetPosDesafio() {
+        
+    }
+
+    @Test
+    public void testEliminarDesafio() {
+        
+    }
+
+    @Test
+    public void testGetUsuarioByNick() {
+        
+    }
+
+    @Test
+    public void testCheckNick() {
+        
+    }
+
+    @Test
+    public void testCheckNumReg() {
+        
+    }
+
+    @Test
+    public void testCheckNickPass() {
+        
+    }
+    
+}
